ebf6936fe4504f6a0936971e4cf15c9bbee9908d
José Miguel De La Concha García <jdelaconcha@mit.local>
2020-04-15 18:34:38 -0500

    Se cambian rutas de qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   name: 'qa',
   // baseUrl: 'https://2u597e7kmf.execute-api.us-east-1.amazonaws.com/test/usuario'
-  baseUrl: 'https://dev-api.mitidentity.com/usuario'
+  baseUrl: 'https://qa-api.mitidentity.com/usuario'
 
 };
 
