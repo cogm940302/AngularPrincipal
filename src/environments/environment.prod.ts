a61954b5f5836e1ca92571679c45cf278fbcba40
Alejandro Omar Pardo Ramirez <opardo@mit.local>
2020-05-15 13:18:39 -0500

    UPDATE se corrige url del api productivo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   name: 'prod',
   // baseUrl: 'https://2u597e7kmf.execute-api.us-east-1.amazonaws.com/test/usuario'
-  baseUrl: 'https://dev-api.mitidentity.com/usuario'
+  baseUrl: 'https://api.mitidentity.com/usuario'
 
 };
