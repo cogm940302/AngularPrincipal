a42c3a7bd8d09ed8dc377c824dcd3def3c9f9d93
Alejandro Omar Pardo Ramirez <opardo@mit.local>
2020-05-26 20:43:35 -0500

    UPDATE - correccion token fingerPrintJs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   name: 'prod',
   // baseUrl: 'https://2u597e7kmf.execute-api.us-east-1.amazonaws.com/test/usuario'
   baseUrl: 'https://api.mitidentity.com/usuario',
-  fingerJsToken: '???',
+  fingerJsToken: 'Q2lmAoHYkmhcMKuhFN5Y',
   secureTouchToken: '???'
 };
