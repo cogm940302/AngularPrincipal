08dcceea731a428085e3c30477805cb8731b56eb
ivanake <edgardoake@hotmail.com>
2020-06-01 14:10:54 -0500

    - Actualización de packages

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+//if (environment.production) {
+//  enableProdMode();
+//}
+
 if (environment.production) {
   enableProdMode();
+  if (window) {
+      window.console.log = () => {};
+  }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
